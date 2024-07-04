7126e7e20989b376cf74736f72047ab5cf54e140
AnuragRaj11 <Araj59519@gmail.com>
2024-07-04 14:20:24 +0530

    vite

---
+++ vite-project/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const card = (props) => {
+  return (
+    <div className='card' style={{overflow:'hidden'}}>
+      <img src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2023/04/react-js-and-vite-js-logo-on-night-sky-background.jpg" alt="" />
+    </div>
+  )
+}
+
+export default card
+props
