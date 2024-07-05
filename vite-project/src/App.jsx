f0eee75a631d77eaf2e34919a620139097c05d3f
AnuragRaj11 <Araj59519@gmail.com>
2024-07-05 20:12:40 +0530

    H

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,16 +3,26 @@ import Card from "./components/Ccard"
 function App() {
   // const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div className="cards">
-        <Card title="card1" descrip="hello react"/>
-        <Card title="card2" descrip="hello react"/>
-        <Card title="card3" descrip="hello react"/>
-        <Card title="card4" descrip="hello react"/>
-      </div>
-    </>
-  )
+
+  // return (
+  //   <>
+  //     <div className="cards">
+  //       <Card title="card1" descrip="hello react"/>
+  //       <Card title="card2" descrip="hello react"/>
+  //       <Card title="card3" descrip="hello react"/>
+  //       <Card title="card4" descrip="hello react"/>
+  //     </div>
+
+
+  //   </>
+  // )
+
+useEffect(() => {
+  
+  
+}, [])
+
+
 }
 
 export default App
