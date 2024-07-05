099279e2c275632717b501bc2f1790b7ad90240d
AnuragRaj11 <Araj59519@gmail.com>
2024-07-05 20:01:11 +0530

    Hook R

--- vite-project/src/components/Ccard.jsx
+++ vite-project/src/components/Ccard.jsx
@@ -4,7 +4,7 @@ import "./Card.css"
 const Ccard = (props) => {
   return (
     <div className='card' style={{overflow:'hidden'}}>
-      <img src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2023/04/react-js-and-vite-js-logo-on-night-sky-background.jpg" alt="" />
+      {/* <img src="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2023/04/react-js-and-vite-js-logo-on-night-sky-background.jpg" alt="" /> */}
       <h1>{props.title}</h1>
       <p>{props.descrip}</p>
     </div>
