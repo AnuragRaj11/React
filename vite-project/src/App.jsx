d965df099bd7d14785b96a7eb2618b5ac2b1c989
AnuragRaj11 <Araj59519@gmail.com>
2024-07-09 00:01:15 +0530

    R

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,18 @@
-import { useRef, useEffect, useState,useMemo,useCallback } from 'react'
+import { useRef, useEffect, useState, useMemo, useCallback } from 'react'
 import Homee from './components/Homee'
 import Navbar from './components/Navbar'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { countercontext } from './context/context'
-import 
+import { useFormAction } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, multiply } from './redux/counter/counterSlice'
 
 function App() {
   // const [count, setCount] = useState(0)
   const [event, handle] = useState({})
   const [showbtn, setData] = useState([])
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
 
   // return (
@@ -51,13 +55,29 @@ function App() {
     },
   ])
 
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   formState: { errors },
+  // } = useForm()
+
   return (
     <>
       {showbtn && <button>rendering occur</button>}
       <input type="text" name="an" id="11" onChange={handleChange} />
 
       <RouterProvider router={router} />
+
+
+      <div>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        Currently count is {count}
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(multiply())}>*</button>
+      </div>
     </>
+
   )
 }
 
